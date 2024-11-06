a3f201ec7628afb3cf4c1f99130c760b88eb378a
Arjun-dev-lab <arjunsk869@gmail.com>
2024-11-06 21:50:11 +0530

    first commit

--- app/src/main/java/com/askproductions/deledhelper/WebViewActivity.kt
+++ app/src/main/java/com/askproductions/deledhelper/WebViewActivity.kt
@@ -10,11 +10,26 @@ class WebViewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_webview)
 
+        // Get the URL passed from the MainActivity or use a default one
         val url = intent.getStringExtra("url") ?: "https://www.google.com"
         val webView = findViewById<WebView>(R.id.webView)
 
+        // Set the WebViewClient to open links inside the WebView
         webView.webViewClient = WebViewClient()
         webView.settings.javaScriptEnabled = true
+
+        // Load the URL in the WebView
         webView.loadUrl(url)
+    }
+
+    override fun onBackPressed() {
+        val webView = findViewById<WebView>(R.id.webView)
+
+        // Check if the WebView can go back in its history
+        if (webView.canGoBack()) {
+            webView.goBack() // Go back to the previous page in history
+        } else {
+            super.onBackPressed() // If not, perform default back action (exit activity)
+        }
     }
 }
